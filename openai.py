f203b6da616a5b8bae0026704cf1139e012f22d5
Steven Alvarenga <andersosuzukius@yahoo.com>
2023-05-04 20:21:01 -0300

    first commit

---
+++ openai.py
@@ -0,0 +1,25 @@
+import openai
+
+# Configurando o API Client
+openai.api_key = 'your-api-key'
+
+# Definindo o prompt
+
+prompt = "What I need study to develop my career in Python?"
+
+# Gerando texto baseado em prompt
+response = openai.Completion.create(
+    engine="davinci"
+    prompt=prompt,
+    max_tokens=50
+)
+
+# Imprimindo o texto gerado
+print(response.choices[0].text)
+
+export OPENAI_API_KEY='your-api-key'
+
+
+
+
+
