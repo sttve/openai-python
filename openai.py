91cd4b4063b4265c0effb59e1104dce34a065f87
Steven Alvarenga <andersosuzukius@yahoo.com>
2023-05-04 20:21:53 -0300

    readme for more info

--- openai.py
+++ openai.py
@@ -1,10 +1,14 @@
+# pip install openai
 import openai
+
+## Terminal: export OPENAI_API_KEY='your-api-key'
+
 
 # Configurando o API Client
 openai.api_key = 'your-api-key'
 
-# Definindo o prompt
 
+# Definindo o prompt
 prompt = "What I need study to develop my career in Python?"
 
 # Gerando texto baseado em prompt
@@ -12,12 +16,11 @@ response = openai.Completion.create(
     engine="davinci"
     prompt=prompt,
     max_tokens=50
-)
+);
 
 # Imprimindo o texto gerado
 print(response.choices[0].text)
 
-export OPENAI_API_KEY='your-api-key'
 
 
 
